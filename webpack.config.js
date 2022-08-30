4d06de9e376aa4f41191b4b20fe812148564ddf2
sumustapha <sumustapha@gmail.com>
2022-08-30 09:45:48 +0100

    v1

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require('path');
 
 module.exports = {
+  mode:'development',
   entry: './src/index.js',
   output: {
    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool:'inline-source-map',
+  devServer:{
+    static:path.resolve(__dirname, 'dist'),
+    port:5501,
+    open: true,
+    hot: true
   },
   module: {
     rules: [
